d33fd1fddc012eb499a5f0e5cdb31b49b6f5fffc
DELL <babutn1977@gmail.com>
2025-11-16 12:55:32 +0530

    First commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Sampletest extends StatefulWidget {
+  const Sampletest({super.key});
+
+  @override
+  State<Sampletest> createState() => _SampletestState();
+}
+
+class _SampletestState extends State<Sampletest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sample ui"),),
+    );
+  }
+}
